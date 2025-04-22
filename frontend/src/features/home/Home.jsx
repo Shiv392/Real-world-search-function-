d68ca59568824d5f653323e3bf9d307fcf7aeeb5
Shiv Soni <sonishiv309@gmail.com>
2025-04-22 21:32:23 +0530

    folder structure done

---
+++ frontend/src/features/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserTable from './components/UserTable';
+
+const Home=()=>{
+return(
+    <>
+    <h1>Home Page</h1>
+    <UserTable />
+    </>
+)
+}
+export default Home;
---
+++ frontend/src/features/home/components/UserTable.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const UserTable=()=>{
+return(
+    <h1>User Table Component</h1>
+)
+}
+export default UserTable;
